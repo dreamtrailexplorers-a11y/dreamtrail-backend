a3569956b049be95c1c02f3761f0088673b8a469
revlinko <contact.revlinko@gmail.com>
2026-08-03 17:41:15 +0530

    add frontend url

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:5173'] : '*',
+  credentials: true
+}));
 app.use(express.json());
 
 // Database Connection
