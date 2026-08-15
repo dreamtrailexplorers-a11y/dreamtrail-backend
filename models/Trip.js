16d6765388803f170a21aad621055a372b3d75c7
revlinko <contact.revlinko@gmail.com>
2026-08-15 12:38:34 +0530

    FOOTER LINK error solve

--- models/Trip.js
+++ models/Trip.js
@@ -48,10 +48,6 @@ const tripSchema = new mongoose.Schema({
     type: String, // Slug or Name of the destination this trip belongs to
     required: true,
   },
-  footerLink: {
-    type: String,
-    default: '',
-  },
   category: {
     type: String, // 'Motorcycle Tours', 'Group Tours', 'Winter Tours', 'Corporate Tours'
     required: true,
